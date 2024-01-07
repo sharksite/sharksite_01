91bc4ff379165a621f45e0da2ab1cf6a65789dbe
aaa <155835909+sharksite@users.noreply.github.com>
2024-01-07 20:08:23 +0900

    ハンバーガーメニューのバッテン挙動を追加

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function () {
 	$('.btn-gNav').on("click", function () {
     $('.gNav').toggleClass('open');  // メニューにopenクラスをつけ外しする
+    $('.btn-gNav').toggleClass('open');  // メニューにopenクラスをつけ外しする
         // メニュー項目のリンクがクリックされたときのイベント
         $('.gNav-menu a').on("click", function () {
           $('.gNav').removeClass('open');  // メニューからopenクラスを削除する
