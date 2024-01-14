0418dcc152bdfdc93f1f9ed6fd1f028bc9c56d6f
aaa <155835909+sharksite@users.noreply.github.com>
2024-01-14 21:45:17 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -145,6 +145,17 @@ function fadeAnime() {
             $(this).removeClass("fadeUp"); // 画面外に出たらfadeUpというクラス名を外す
         }
     });
+    // 左からふわっ
+    $(".fadeLeftTrigger").each(function () {
+        var elemPos = $(this).offset().top - 50; //要素より、50px上の
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight) {
+            $(this).addClass("fadeLeft"); // 画面内に入ったらfadeUpというクラス名を追記
+        } else {
+            $(this).removeClass("fadeLeft"); // 画面外に出たらfadeUpというクラス名を外す
+        }
+    });
 }
 
 // 画面をスクロールをしたら動かしたい場合の記述
